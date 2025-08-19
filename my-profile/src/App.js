dbe7493e9e9b29d68d6b1602eea6baf04eb4a18c
thaoly <thaolythly@gmail.com>
2025-08-19 21:15:55 +0700

    first commit

---
+++ my-profile/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Thông tin cá nhân</h1>
+      <p>Họ và tên: Trần Thị Thảo Ly</p>
+      <p> Sinh viên năm 4</p>
+      <p>Ngày sinh: 01/12/2003</p>
+      <p>Email: thaoly112@gmail.com</p>
+    </div>
+  );
+}
+
+export default App;
